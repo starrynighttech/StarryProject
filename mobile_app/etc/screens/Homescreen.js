a27016d34234fa0c60ca9b6e50d2513e5e348abc
starrynighttech <starrynight8531@gmail.com>
2026-06-19 23:23:23 +0200

    Create Homescreen.js

---
+++ mobile_app/etc/screens/Homescreen.js
@@ -0,0 +1,22 @@
+import React from "react"
+import {
+ View,
+ Text
+} from "react-native"
+
+export default function HomeScreen(){
+
+return(
+
+<View>
+
+<Text>
+Welcome To
+Starry Mobile Space
+</Text>
+
+</View>
+
+)
+
+}
