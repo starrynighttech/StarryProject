dadf05857f4ec1fbe73cc44239eecc08e3e30c6b
starrynighttech <starrynight8531@gmail.com>
2026-06-20 15:13:35 +0200

    Update tracking.js

--- backend/routes/tracking.js
+++ backend/routes/tracking.js
@@ -1,14 +1,22 @@
-const router = require("express").Router()
+const router =
+require("express").Router()
 
 const Tracking =
 require("../models/Tracking")
 
-router.get("/:code", async(req,res)=>{
+router.get("/:code",
+async(req,res)=>{
 
   const tracking =
   await Tracking.findOne({
     internalCode:req.params.code
   })
+
+  if(!tracking){
+    return res.status(404).json({
+      message:"Tracking not found"
+    })
+  }
 
   res.json(tracking)
 
