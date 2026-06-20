de1e665b98b9312007031fcef81b4bd33481fad8
starrynighttech <starrynight8531@gmail.com>
2026-06-20 13:02:05 +0200

    Update admin.js

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
 
   if (req.user.role !== "admin") {
     return res.status(403).json({
-      message: "Access denied"
+      message: "Admin access required"
     })
   }
 
