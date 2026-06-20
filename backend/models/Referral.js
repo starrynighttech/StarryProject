e1f23e741e120462a77f6c1dbbb9c0eb193555dd
starrynighttech <starrynight8531@gmail.com>
2026-06-20 14:33:52 +0200

    Update Referral.js

--- backend/models/Referral.js
+++ backend/models/Referral.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose")
 
+const ReferralSchema = new mongoose.Schema({
+  referrerId: String,
+  referredUserId: String,
+
+  rewardAmount: {
+    type: Number,
+    default: 0
+  },
+
+  status: {
+    type: String,
+    default: "pending"
+  }
+
+},{
+  timestamps:true
+})
+
+module.exports =
+mongoose.model(
+  "Referral",
+  ReferralSchema
+)
