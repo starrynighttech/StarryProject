e514ccee370067f85ba0320e412a63427273e81c
starrynighttech <starrynight8531@gmail.com>
2026-06-20 14:13:54 +0200

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,1 +1,37 @@
+const router = require("express").Router()
 
+const User =
+require("../models/User")
+
+const Order =
+require("../models/Order")
+
+router.get("/stats", async(req,res)=>{
+
+  const users =
+  await User.countDocuments()
+
+  const orders =
+  await Order.countDocuments()
+
+  const revenueOrders =
+  await Order.find({
+    paymentStatus:"paid"
+  })
+
+  const revenue =
+  revenueOrders.reduce(
+    (sum,item)=>
+      sum + item.total,
+    0
+  )
+
+  res.json({
+    users,
+    orders,
+    revenue
+  })
+
+})
+
+module.exports = router
