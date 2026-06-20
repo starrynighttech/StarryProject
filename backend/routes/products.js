15254edee5fb5a11e66e7db03cc1ee4dfe43c7e7
starrynighttech <starrynight8531@gmail.com>
2026-06-20 13:12:11 +0200

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,13 +1,48 @@
 const router = require("express").Router()
 
-const Product = require("../models/Product")
+const Product =
+require("../models/Product")
 
 router.get("/", async(req,res)=>{
 
   const products =
   await Product.find()
+  .populate("category")
 
   res.json(products)
+
+})
+
+router.get("/featured", async(req,res)=>{
+
+  const products =
+  await Product.find({
+    featured:true
+  })
+
+  res.json(products)
+
+})
+
+router.get("/:id", async(req,res)=>{
+
+  const product =
+  await Product.findById(
+    req.params.id
+  ).populate("category")
+
+  res.json(product)
+
+})
+
+router.post("/", async(req,res)=>{
+
+  const product =
+  await Product.create(
+    req.body
+  )
+
+  res.status(201).json(product)
 
 })
 
