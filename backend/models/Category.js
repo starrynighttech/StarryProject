7127b650bc7eb23320b1004b2931fd195933eb1d
starrynighttech <starrynight8531@gmail.com>
2026-06-20 13:11:04 +0200

    Create Category.js

---
+++ backend/models/Category.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const CategorySchema =
+new mongoose.Schema({
+
+  name:{
+    type:String,
+    required:true,
+    unique:true
+  },
+
+  image:String
+
+},{
+  timestamps:true
+})
+
+module.exports =
+mongoose.model(
+  "Category",
+  CategorySchema
+)
