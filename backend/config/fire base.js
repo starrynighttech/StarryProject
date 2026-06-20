84f539f4b4309b67537e4c258d570c9a113100ae
starrynighttech <starrynight8531@gmail.com>
2026-06-20 16:46:52 +0200

    Create fire base.js

---
+++ backend/config/fire base.js
@@ -0,0 +1,14 @@
+const admin =
+require("firebase-admin")
+
+const serviceAccount =
+require("../firebase-key.json")
+
+admin.initializeApp({
+  credential:
+  admin.credential.cert(
+    serviceAccount
+  )
+})
+
+module.exports = admin
