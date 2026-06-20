3b94391eae147204d0c3a57ea4bdf722ddcbb18d
starrynighttech <starrynight8531@gmail.com>
2026-06-20 14:40:11 +0200

    Update AdCampaign.js

--- backend/models/AdCampaign.js
+++ backend/models/AdCampaign.js
@@ -1,1 +1,35 @@
+const mongoose =
+require("mongoose")
 
+const AdCampaignSchema =
+new mongoose.Schema({
+
+  businessId:String,
+
+  title:String,
+
+  videoUrl:String,
+
+  budget:Number,
+
+  rewardPerView:Number,
+
+  views:{
+    type:Number,
+    default:0
+  },
+
+  active:{
+    type:Boolean,
+    default:true
+  }
+
+},{
+  timestamps:true
+})
+
+module.exports =
+mongoose.model(
+  "AdCampaign",
+  AdCampaignSchema
+)
